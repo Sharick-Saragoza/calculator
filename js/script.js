cbaf73b35834cf06232fdb297d8982b3830a5bb2
Sharick <150659105+Sharick-Saragoza@users.noreply.github.com>
2025-11-28 23:51:26 +0100

    String variables can now be calculated by being turned into a number.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log("script is working");
+function add(num1, num2) {
+    return Number(num1) + Number(num2);
+}
+
+function subtract(num1, num2) {
+    return Number(num1) - Number(num2);
+}
+
+function multiply(num1, num2) {
+    return Number(num1) * Number(num2);
+}
+
+function divide(num1, num2) {
+    return Number(num1) / Number(num2);
+}
+
+function operate(num1, operator, num2) {
+    if (operator == "+") {
+        return add(num1, num2);
+    } else if (operator == "-") {
+        return subtract(num1, num2);
+    } else if (operator == "x") {
+        return multiply(num1, num2);
+    } else if (operator == "/") {
+        return divide(num1, num2);
+    }
+}
+
+// console.log(operate("20", "+", "20"));
