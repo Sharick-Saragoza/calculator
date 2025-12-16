f932fd99efc99a51777d4b20b2ebc55d95eb296a
Sharick <sharickson9@live.nl>
2025-12-16 16:38:26 +0100

    Removed almost everything in the Javascript file to remake the calculator without bugs. The code is much cleaner now. You can only add number to the display right now.

--- js/script.js
+++ js/script.js
@@ -1,117 +1,33 @@
-// Numbers
-const btn0 = document.getElementById("zero");
-btn0.addEventListener("click", updateDisplayInput);
+// Display
+const display = document.querySelector(".display");
 
-const btn1 = document.getElementById("one");
-btn1.addEventListener("click", updateDisplayInput);
+let previousNumber = "";
+let currentNumber = "";
+let currentOperator = "";
 
-const btn2 = document.getElementById("two");
-btn2.addEventListener("click", updateDisplayInput);
-
-const btn3 = document.getElementById("three");
-btn3.addEventListener("click", updateDisplayInput);
-
-const btn4 = document.getElementById("four");
-btn4.addEventListener("click", updateDisplayInput);
-
-const btn5 = document.getElementById("five");
-btn5.addEventListener("click", updateDisplayInput);
-
-const btn6 = document.getElementById("six");
-btn6.addEventListener("click", updateDisplayInput);
-
-const btn7 = document.getElementById("seven");
-btn7.addEventListener("click", updateDisplayInput);
-
-const btn8 = document.getElementById("eight");
-btn8.addEventListener("click", updateDisplayInput);
-
-const btn9 = document.getElementById("nine");
-btn9.addEventListener("click", updateDisplayInput);
-
-// First number & chosen operator. 
-let firstNumber = null;
-let chosenOperator = null;
-
-// Operators
-const btnPlus = document.getElementById("plus");
-btnPlus.addEventListener("click", () => {
-    firstNumber += Number(display.textContent);
-    chosenOperator = "+";
-    clearDisplayInput();
-});
-
-const btnMinus = document.getElementById("minus");
-btnMinus.addEventListener("click", () => {
-    firstNumber += Number(display.textContent);
-    chosenOperator = "-";
-    clearDisplayInput();
-});
-
-const btnMultiply = document.getElementById("multiply");
-btnMultiply.addEventListener("click", () => {
-    firstNumber += Number(display.textContent);
-    chosenOperator = "x";
-    clearDisplayInput();
-});
-
-const btnDivide = document.getElementById("divide");
-btnDivide.addEventListener("click", () => {
-    firstNumber += Number(display.textContent);
-    chosenOperator = "/";
-    clearDisplayInput();
-});
-
-const btnEquals = document.getElementById("equals");
-btnEquals.addEventListener("click", () => {
-    display.textContent = operate(firstNumber, chosenOperator, Number(displayInput));
-})
-
-const btnAC = document.getElementById("ac").addEventListener("click", allClear);
-
-let displayInput = "";
-let display = document.querySelector(".display");
-display.textContent = displayInput;
-
-function updateDisplayInput() {
-    display.textContent = displayInput += this.textContent;
+// Append number
+function appendNumber(number) {
+    currentNumber += number;
+    refreshDisplay()
 }
 
-function allClear() {
-    displayInput = "";
-    display.textContent = "";
-    firstNumber = null;
+// Refresh display
+function refreshDisplay() {
+    display.textContent = currentNumber;
 }
 
-function clearDisplayInput() {
-    displayInput = "";
-    display.textContent = "";
-}
+// function add(num1, num2) {
 
-function add(num1, num2) {
-    return Number(num1) + Number(num2);
-}
+// }
 
-function subtract(num1, num2) {
-    return Number(num1) - Number(num2);
-}
+// function minus(num1, num2) {
 
-function multiply(num1, num2) {
-    return Number(num1) * Number(num2);
-}
+// }
 
-function divide(num1, num2) {
-    return Number(num1) / Number(num2);
-}
+// function multiply(num1, num2) {
 
-function operate(num1, operator, num2) {
-    if (operator == "+") {
-        return add(num1, num2);
-    } else if (operator == "-") {
-        return subtract(num1, num2);
-    } else if (operator == "x") {
-        return multiply(num1, num2);
-    } else if (operator == "/") {
-        return divide(num1, num2);
-    }
-}
+// }
+
+// function divide(num1, num2) {
+
+// }
